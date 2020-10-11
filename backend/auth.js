cf69bf1af2baab97172865e288dc646b356f2b33
dplamenov <dimitar.plamenov@gmail.com>
2020-10-11 17:00:28 +0300

    check auth-token when user login

--- backend/auth.js
+++ backend/auth.js
@@ -1,5 +1,6 @@
 const jwt = require('jsonwebtoken');
 const User = require('./models/user');
+const TokenBlacklist = require('./models/tokenBlacklist');
 const {authCookie} = require('./config');
 
 const secret = 'b7d4503acc8d249049e66d4f8936ac5e';
@@ -29,8 +30,11 @@ function auth(req, res, next) {
     const {login: isLogin, userId} = getTokenFromReq(req);
 
     if (isLogin) {
-        User.findById(userId)
-            .then(user => {
+        Promise.all([User.findById(userId), isTokenValid(req.cookies[authCookie])])
+            .then(([user, tokenBlacklisted]) => {
+                if (tokenBlacklisted) {
+                    return next('no user');
+                }
                 req.user = user;
                 return next();
             })
@@ -41,5 +45,15 @@ function auth(req, res, next) {
     next('no user');
 }
 
+
+function isTokenValid(token) {
+    return new Promise(((resolve, reject) => {
+        TokenBlacklist.findOne({token})
+            .then(tokenData => {
+                resolve(!!tokenData);
+            })
+            .catch(reject);
+    }));
+}
 
 module.exports = {generateAuthToken, auth, getUserId};
--- backend/router.js
+++ backend/router.js
@@ -5,7 +5,7 @@ const controllers = require('./controllers');
 
 router.post('/user/login', controllers.user.login);
 router.post('/user/register', controllers.user.register);
-router.get('/user/logout', controllers.user.logout);
+router.get('/user/logout', auth, controllers.user.logout);
 router.post('/product/create', auth, controllers.product.createProduct);
 
 module.exports = router;
