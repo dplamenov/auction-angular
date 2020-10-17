bb05f43c82bcacf780cfed4c1a5b6b23bead711c
dplamenov <dimitar.plamenov@gmail.com>
2020-10-17 20:06:57 +0300

    update user model

--- backend/models/product.js
+++ backend/models/product.js
@@ -1,23 +1,27 @@
 const mongoose = require('mongoose');
 
 const ProductSchema = mongoose.Schema({
-    title: {
-        type: String,
-        required: true
-    },
-    description: {
-        type: String,
-        required: true
-    },
-    creator: {
-        type: mongoose.Types.ObjectId,
-        ref: 'User',
-        required: true
-    },
-    imageName: {
-        type: String,
-        required: true
-    }
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  creator: {
+    type: mongoose.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  imageName: {
+    type: String,
+    required: true
+  },
+  createTime: {
+    type: String,
+    default: Date.now
+  }
 });
 
 module.exports = mongoose.model('Product', ProductSchema);
