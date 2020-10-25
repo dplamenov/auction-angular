faee06e85a0d3b8d948943293ecc0ce80e6df5aa
dplamenov <dimitar.plamenov@gmail.com>
2020-10-25 13:11:18 +0200

    create user interface

--- frondend/src/app/user/user.service.ts
+++ frondend/src/app/user/user.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class UserService {
+  private apiPath = 'http://localhost:3000/api/';
+  private user: User;
 
   constructor() { }
 }
