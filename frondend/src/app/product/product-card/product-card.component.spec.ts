66ae1be8d53e0efab59235212cc73b0433e4b805
dplamenov <dimitar.plamenov@gmail.com>
2020-11-11 11:23:26 +0200

    product-card component

--- frondend/src/app/product/product-card/product-card.component.spec.ts
+++ frondend/src/app/product/product-card/product-card.component.spec.ts
@@ -1,6 +1,6 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { ProductCardComponent } from './product-card.component';
+import {ProductCardComponent} from './product-card.component';
 
 describe('ProductCardComponent', () => {
   let component: ProductCardComponent;
@@ -8,14 +8,26 @@ describe('ProductCardComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ProductCardComponent ]
+      declarations: [ProductCardComponent]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ProductCardComponent);
     component = fixture.componentInstance;
+    component.product = {
+      _id: '5fa92651df489d0ddf552342',
+      title: 'product title1 4 1525 15 152',
+      description: 'test',
+      endTime: '1603014589212',
+      startPrice: '4104.155',
+      creator: '5f8309254cd133105880f9a2',
+      createTime: '1604920913222',
+      isOwner: false,
+      endString: '21.12.2020',
+      image: 'kw.jpg'
+    };
     fixture.detectChanges();
   });
 
