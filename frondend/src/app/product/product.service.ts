f84fa2faff35620bf441702a10f956a82861e451
dplamenov <dimitar.plamenov@gmail.com>
2020-11-11 10:22:09 +0200

    product service tests

--- frondend/src/app/product/product.service.ts
+++ frondend/src/app/product/product.service.ts
@@ -2,7 +2,7 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Product} from './product';
 
-@Injectable()
+@Injectable({providedIn: 'root'})
 export class ProductService {
   constructor(private httpClient: HttpClient) {
   }
