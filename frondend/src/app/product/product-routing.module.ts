26670eebaf7a9b41428abdbfc3b2eb16e424f383
dplamenov <dimitar.plamenov@gmail.com>
2020-11-13 16:41:02 +0200

    add 'all products' link in navbar

--- frondend/src/app/product/product-routing.module.ts
+++ frondend/src/app/product/product-routing.module.ts
@@ -3,9 +3,11 @@ import {Routes, RouterModule} from '@angular/router';
 import {CreateComponent} from './create/create.component';
 import {AuthGuard} from '../auth.guard';
 import {ProductDetailsComponent} from './product-details/product-details.component';
+import {AllComponent} from './all/all.component';
 
 const routes: Routes = [
   {path: 'create', component: CreateComponent, canActivate: [AuthGuard]},
+  {path: 'all', component: AllComponent},
   {path: ':productId', component: ProductDetailsComponent}
 ];
 
