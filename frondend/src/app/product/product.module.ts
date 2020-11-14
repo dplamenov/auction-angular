22862f6b863ecf446a994ce32135665060ecb265
dplamenov <dimitar.plamenov@gmail.com>
2020-11-14 14:58:33 +0200

    use mat-paginator

--- frondend/src/app/product/product.module.ts
+++ frondend/src/app/product/product.module.ts
@@ -1,23 +1,25 @@
 import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {LatestComponent} from './latest/latest.component';
-import {CreateComponent} from './create/create.component';
-import {HttpClientModule} from '@angular/common/http';
 import {HTTP_INTERCEPTORS} from '@angular/common/http';
-import {DefaultInterceptor} from '../default.interceptor';
-import {ProductService} from './product.service';
+import {CommonModule} from '@angular/common';
+import {HttpClientModule} from '@angular/common/http';
 import {ReactiveFormsModule} from '@angular/forms';
-import {ProductCardComponent} from './product-card/product-card.component';
-import {ProductDetailsComponent} from './product-details/product-details.component';
 import {RouterModule} from '@angular/router';
 
-import {MatDatepickerModule} from '@angular/material/datepicker'; //1
-import {MatFormFieldModule} from '@angular/material/form-field'; //2
-import {MatNativeDateModule} from '@angular/material/core'; // 3
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatNativeDateModule} from '@angular/material/core';
 import {MatInputModule} from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
+import {MatPaginatorModule} from '@angular/material/paginator';
+
+import {LatestComponent} from './latest/latest.component';
+import {CreateComponent} from './create/create.component';
+import {AllComponent} from './all/all.component';
+import {ProductService} from './product.service';
+import {ProductCardComponent} from './product-card/product-card.component';
+import {ProductDetailsComponent} from './product-details/product-details.component';
+import {DefaultInterceptor} from '../default.interceptor';
 import {ProductRoutingModule} from './product-routing.module';
-import { AllComponent } from './all/all.component';
 
 @NgModule({
   declarations: [LatestComponent, CreateComponent, ProductCardComponent, ProductDetailsComponent, AllComponent],
@@ -27,7 +29,8 @@ import { AllComponent } from './all/all.component';
     ReactiveFormsModule,
     RouterModule,
     MatFormFieldModule, MatDatepickerModule, MatNativeDateModule, MatInputModule, MatButtonModule,
-    ProductRoutingModule],
+    ProductRoutingModule,
+    MatPaginatorModule],
   exports: [LatestComponent, CreateComponent],
   providers: [
     {
