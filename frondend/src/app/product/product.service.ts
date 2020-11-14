ecfaf55387c26b8e770c62f73e866c4f4836ab82
dplamenov <dimitar.plamenov@gmail.com>
2020-11-14 13:09:56 +0200

    bug fix in latest component

--- frondend/src/app/product/product.service.ts
+++ frondend/src/app/product/product.service.ts
@@ -1,14 +1,23 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Product} from './product';
+import {environment} from '../../environments/environment';
+import {map} from 'rxjs/operators';
 
 @Injectable({providedIn: 'root'})
 export class ProductService {
   constructor(private httpClient: HttpClient) {
   }
 
+  static addImagePath(product) {
+    const {imagePath} = environment;
+    product.image = `${imagePath}${product._id}.png`;
+
+    return product;
+  }
+
   getLatestProducts() {
-    return this.httpClient.get<Product[]>(`product/latest`);
+    return this.httpClient.get<Product[]>(`product/latest`).pipe(map(ProductService.addImagePath));
   }
 
   create(product) {
