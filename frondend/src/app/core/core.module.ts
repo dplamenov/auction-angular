467eb4a4b93feb367079ea2632d7c0148651630f
dplamenov <dimitar.plamenov@gmail.com>
2020-11-18 13:29:10 +0200

    create and use separate header component

--- frondend/src/app/core/core.module.ts
+++ frondend/src/app/core/core.module.ts
@@ -3,15 +3,16 @@ import {CommonModule} from '@angular/common';
 import {NotFoundComponent} from './not-found/not-found.component';
 import {RouterModule} from '@angular/router';
 import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
 
 
 @NgModule({
-  declarations: [NotFoundComponent, FooterComponent],
+  declarations: [NotFoundComponent, FooterComponent, HeaderComponent],
   imports: [
     CommonModule,
     RouterModule
   ],
-  exports: [NotFoundComponent, FooterComponent]
+  exports: [NotFoundComponent, FooterComponent, HeaderComponent]
 })
 export class CoreModule {
 }
