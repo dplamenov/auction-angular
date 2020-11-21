078bbcf6878af39da3e0fc9f506162c6bc4b68b2
dplamenov <dimitar.plamenov@gmail.com>
2020-11-21 20:52:41 +0200

    add route for edit product

--- frondend/src/app/product/product-routing.module.ts
+++ frondend/src/app/product/product-routing.module.ts
@@ -5,12 +5,14 @@ import {AuthGuard} from '../auth.guard';
 import {DetailsComponent} from './details/details.component';
 import {AllComponent} from './all/all.component';
 import {LatestComponent} from './latest/latest.component';
+import {EditComponent} from './edit/edit.component';
 
 const routes: Routes = [
   {path: 'latest', component: LatestComponent},
   {path: 'create', component: CreateComponent, canActivate: [AuthGuard]},
   {path: 'all', component: AllComponent},
-  {path: ':productId', component: DetailsComponent}
+  {path: ':productId', component: DetailsComponent},
+  {path: 'edit/:productId', component: EditComponent, canActivate: [AuthGuard]}
 ];
 
 @NgModule({
