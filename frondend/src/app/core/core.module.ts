f087da8b86c71336d6500dcddd913b246a5179aa
dplamenov <dimitar.plamenov@gmail.com>
2020-11-22 20:24:19 +0200

    create and use shorten pipe

--- frondend/src/app/core/core.module.ts
+++ frondend/src/app/core/core.module.ts
@@ -4,16 +4,17 @@ import {NotFoundComponent} from './not-found/not-found.component';
 import {RouterModule} from '@angular/router';
 import {FooterComponent} from './footer/footer.component';
 import {HeaderComponent} from './header/header.component';
-import { MinDirective } from './min.directive';
+import {MinDirective} from './min.directive';
+import {ShortenPipe} from './shorten.pipe';
 
 
 @NgModule({
-  declarations: [NotFoundComponent, FooterComponent, HeaderComponent, MinDirective],
+  declarations: [NotFoundComponent, FooterComponent, HeaderComponent, MinDirective, ShortenPipe],
   imports: [
     CommonModule,
     RouterModule
   ],
-    exports: [NotFoundComponent, FooterComponent, HeaderComponent, MinDirective]
+  exports: [NotFoundComponent, FooterComponent, HeaderComponent, MinDirective, ShortenPipe]
 })
 export class CoreModule {
 }
