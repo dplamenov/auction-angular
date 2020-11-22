95a17d295c834dc435641c993d6157cb768d48da
dplamenov <dimitar.plamenov@gmail.com>
2020-11-22 20:19:31 +0200

    details component validate bug fix

--- frondend/src/app/core/min.directive.ts
+++ frondend/src/app/core/min.directive.ts
@@ -6,11 +6,11 @@ import {AbstractControl, NG_VALIDATORS, Validator} from '@angular/forms';
   providers: [{provide: NG_VALIDATORS, useExisting: MinDirective, multi: true}]
 })
 export class MinDirective implements Validator {
-  @Input('appMin') min: number;
+  @Input('appMin') min: string | number;
 
   validate(control: AbstractControl): { [key: string]: any } | null {
-    const value = Number(control.value);
-    if (value <= this.min) {
+    const value = +control.value;
+    if (value <= +this.min) {
       return {bid: {value}};
     }
     return null;
