acdaea1a62c6d4f3327dbd58a696bf3a8131d028
dplamenov <dimitar.plamenov@gmail.com>
2020-11-24 08:31:27 +0200

    add titlePrefix to environment variable

--- frondend/src/environments/environment.prod.ts
+++ frondend/src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   apiServer: 'http://auction.sharkdev.eu/api/api/',
-  imagePath: 'http://auction.sharkdev.eu/api/images'
+  imagePath: 'http://auction.sharkdev.eu/api/images',
+  titlePrefix: 'Online Auction'
 };
--- frondend/src/environments/environment.ts
+++ frondend/src/environments/environment.ts
@@ -5,7 +5,8 @@
 export const environment = {
   production: false,
   apiServer: 'http://localhost:3000/api/',
-  imagePath: 'http://localhost:3000/images/'
+  imagePath: 'http://localhost:3000/images/',
+  titlePrefix: 'Online Auction'
 };
 
 /*
