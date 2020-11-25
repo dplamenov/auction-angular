08f18e2f88bcbc2d60aadcf087fb7883a549bb89
dplamenov <dimitar.plamenov@gmail.com>
2020-11-25 16:55:59 +0200

    fixed navigation in user module

--- frondend/src/app/user/user-routing.module.ts
+++ frondend/src/app/user/user-routing.module.ts
@@ -2,12 +2,13 @@ import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {LoginComponent} from './login/login.component';
 import {RegisterComponent} from './register/register.component';
+import {ProfileComponent} from './profile/profile.component';
 import {AuthGuard} from '../shared/guards/auth.guard';
 
 const routes: Routes = [
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
-  {path: 'profile', component: RegisterComponent, canActivate: [AuthGuard]}
+  {path: 'profile', component: ProfileComponent, canActivate: [AuthGuard]}
 ];
 
 @NgModule({
