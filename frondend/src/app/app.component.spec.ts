c9aae153fa4a00f6ced3c05763d79510662ab363
dplamenov <dimitar.plamenov@gmail.com>
2020-11-27 14:07:50 +0200

    write test for footer component
    write test for footer component

--- frondend/src/app/app.component.spec.ts
+++ frondend/src/app/app.component.spec.ts
@@ -1,6 +1,6 @@
-import { TestBed, async } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './app.component';
+import {TestBed, async} from '@angular/core/testing';
+import {RouterTestingModule} from '@angular/router/testing';
+import {AppComponent} from './app.component';
 import {HttpClient, HttpClientModule} from '@angular/common/http';
 
 describe('AppComponent', () => {
@@ -29,9 +29,10 @@ describe('AppComponent', () => {
     expect(app.notification).toEqual(undefined);
   });
 
-    it('should have router-outlet', () => {
+  it('should have router-outlet', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
     expect(compiled.getElementsByTagName('router-outlet')[0]).toBeTruthy();
-  });});
+  });
+});
--- frondend/src/app/core/footer/footer.component.spec.ts
+++ frondend/src/app/core/footer/footer.component.spec.ts
@@ -22,4 +22,8 @@ describe('FooterComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('content should contains "auction system"', () => {
+    expect(fixture.nativeElement.getElementsByTagName('p')[0].textContent).toContain('auction system');
+  });
 });
