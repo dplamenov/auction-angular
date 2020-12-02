2a217a834c623d81808fccd799de69f810ac7e66
dplamenov <dimitar.plamenov@gmail.com>
2020-12-02 14:59:43 +0200

    update product details component creator link
    update product details component creator link

--- frondend/src/app/user/user-routing.module.ts
+++ frondend/src/app/user/user-routing.module.ts
@@ -8,7 +8,8 @@ import {AuthGuard} from '../shared/guards/auth.guard';
 const routes: Routes = [
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
-  {path: 'profile', component: ProfileComponent, canActivate: [AuthGuard]}
+  {path: 'profile', component: ProfileComponent, canActivate: [AuthGuard]},
+  {path: 'profile/:userId', component: ProfileComponent}
 ];
 
 @NgModule({
