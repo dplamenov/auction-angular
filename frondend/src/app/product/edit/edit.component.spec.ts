bf5c7ba04b56fe8b087488a84d1d85a1c1e98220
dplamenov <dimitar.plamenov@gmail.com>
2020-12-03 18:41:12 +0200

    edit component test
    edit component test

--- frondend/src/app/product/edit/edit.component.spec.ts
+++ frondend/src/app/product/edit/edit.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { EditComponent } from './edit.component';
+import {HttpClientModule} from '@angular/common/http';
+import {RouterTestingModule} from '@angular/router/testing';
 
 describe('EditComponent', () => {
   let component: EditComponent;
@@ -8,7 +10,8 @@ describe('EditComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ EditComponent ]
+      declarations: [ EditComponent ],
+      imports: [HttpClientModule, RouterTestingModule]
     })
     .compileComponents();
   }));
