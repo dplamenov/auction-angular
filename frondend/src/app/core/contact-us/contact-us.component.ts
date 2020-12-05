f894aac05cd6d2a0d870bbcdb860fb2cc3863445
dplamenov <dimitar.plamenov@gmail.com>
2020-12-05 09:48:32 +0200

    contact form handle submit
    contact form handle submit

--- frondend/src/app/core/contact-us/contact-us.component.ts
+++ frondend/src/app/core/contact-us/contact-us.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {UserService} from '../../user/user.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-contact-us',
@@ -13,9 +14,13 @@ export class ContactUsComponent implements OnInit {
     return user ? user.email : '';
   }
 
-  constructor(private userService: UserService) { }
+  constructor(private userService: UserService, private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  submitForm(form){
+    this.router.navigate(['/'], {queryParams: {notification: 'Your message has been sent.'}});
   }
 
 }
