f7a2fd5f6f9b403c0be6c9e4ba6c8090545ce190
dplamenov <dimitar.plamenov@gmail.com>
2020-12-05 09:28:40 +0200

    fill contact form with user email
    fill contact form with user email

--- frondend/src/app/core/contact-us/contact-us.component.ts
+++ frondend/src/app/core/contact-us/contact-us.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {UserService} from '../../user/user.service';
 
 @Component({
   selector: 'app-contact-us',
@@ -7,7 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactUsComponent implements OnInit {
 
-  constructor() { }
+  get email(){
+    const user = this.userService.user;
+    return user ? user.email : '';
+  }
+
+  constructor(private userService: UserService) { }
 
   ngOnInit(): void {
   }
--- frondend/src/app/core/core.module.ts
+++ frondend/src/app/core/core.module.ts
@@ -7,6 +7,7 @@ import {HeaderComponent} from './header/header.component';
 import {MatSlideToggleModule} from '@angular/material/slide-toggle';
 import {SharedModule} from '../shared/shared.module';
 import { ContactUsComponent } from './contact-us/contact-us.component';
+import {FormsModule} from '@angular/forms';
 
 
 @NgModule({
@@ -15,7 +16,8 @@ import { ContactUsComponent } from './contact-us/contact-us.component';
     CommonModule,
     RouterModule,
     MatSlideToggleModule,
-    SharedModule
+    SharedModule,
+    FormsModule
   ],
   exports: [NotFoundComponent, FooterComponent, HeaderComponent, ContactUsComponent]
 })
