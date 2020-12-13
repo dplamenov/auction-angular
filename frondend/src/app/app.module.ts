9f389a88b5663d51e3881b41ad74d0b1f90030e5
dplamenov <dimitar.plamenov@gmail.com>
2020-12-13 13:07:40 +0200

    Update app.module.ts

--- frondend/src/app/app.module.ts
+++ frondend/src/app/app.module.ts
@@ -5,8 +5,6 @@ import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {ReactiveFormsModule} from '@angular/forms';
-import {UserModule} from './user/user.module';
-import {ProductModule} from './product/product.module';
 import {CoreModule} from './core/core.module';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {CustomTitleService} from './core/custom-title.service';
